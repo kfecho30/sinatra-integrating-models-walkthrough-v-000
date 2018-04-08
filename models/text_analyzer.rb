f050eb5dc107a2fab21a13e3fb88d7c9b9941d4d
Kelsey Fecho <kfecho30@gmail.com>
2018-04-08 00:49:05 +0000

    automatically backed up by learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,17 +1,17 @@
 class TextAnalyzer
-  attr_reader :text 
+  attr_reader :text
 
   def initialize(text)
     @text = text.downcase
   end
 
-  def count_of_words 
+  def count_of_words
     words = text.split(" ")
-    words.count 
+    words.count
   end
 
   def count_of_vowels
-    text.scan(/[aeiou]/).count 
+    text.scan(/[aeiou]/).count
   end
 
   def count_of_consonants
